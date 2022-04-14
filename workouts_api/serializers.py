d3a2d10ec081740848492108f772b119e81226d2
Thomas Johnson <thomasj.johnson20@gmail.com>
2022-04-14 17:43:49 -0500

    fix

--- workouts_api/serializers.py
+++ workouts_api/serializers.py
@@ -3,4 +3,4 @@ from .models import Workout
 class WorkoutSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Workout # tell django which model to use
-        fields = ('id', 'date', 'excercises', 'notes') # tell django which fields to include
+        fields = ('id', 'date', 'exercises', 'notes') # tell django which fields to include
