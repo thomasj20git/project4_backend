7f9deeedf528f262e5e106adfc02cfeab209220b
Thomas Johnson <thomasj.johnson20@gmail.com>
2022-04-15 10:32:06 -0500

    fix

--- workouts_api/urls.py
+++ workouts_api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.WorkoutList.as_view(), name='workout_list'), # api/contacts will be routed to the ContactList view for handling
-    path('<int:pk>', views.WorkoutDetail.as_view(), name='workout_detail'), # api/contacts will be routed to the ContactDetail view for handling
+    path('<int:pk>/', views.WorkoutDetail.as_view(), name='workout_detail'), # api/contacts will be routed to the ContactDetail view for handling
 ]
